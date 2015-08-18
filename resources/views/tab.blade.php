1066bcdc505c5847e61a03cd8d5acf142ec686f6
Mihai Barbu <xwave21@gmail.com>
2015-08-18 15:25:52 +0300

    Bottom and form

--- resources/views/tab.blade.php
+++ resources/views/tab.blade.php
@@ -6,8 +6,8 @@
         <meta name="description" content="">
         <title>Soho</title>
         
-        <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}"> -->
+        <!-- <link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}"> -->
         
         <link rel="stylesheet" type="text/css" href="/css/main.css">
     </head>
@@ -58,6 +58,31 @@
             la alegere, un router wifi sau un rucsac. Mai multe detalii în magazinele partenere Canon.</p>
 
             <div class="stripe-red"></div>
+
+            <img class="tabel" src="/img/tabel.png">
+
+            <p class="text-bottom">*Imaginile produselor sunt cu titlu de prezentare</p>
+            <p class="text-bottom-sec">Perioada promotiei 1 august - 30 septembrie 2015</p>
+
+            <img class="elipse" src="/img/ellipse-bottom.png">
+            <span class="abonare">Doreşti să fii la curent cu noutăţile Canon? Abonează-te la newsletter-ul nostru, mai jos.</span>
+
+            <div class="form-inscriere">
+                <form action="/" data-form="true" method="post">
+                    <label for="nume" class="forlabel">Nume și Prenume</label>
+                    <input type="text"  value="{{ old('nume') }}" data-nume="true" name="nume" class="form-control numebox"><br /><br />
+
+                    <label for="email" class="forlabel emaillabel">Email</label>
+                    <input type="email" value="{{ old('email') }}" data-email="true" name="email" class="form-control emailbox">
+                    <br>
+
+                    <input type="checkbox" class="checkbox2" name="contact" data-checkbox="true" value="1" checked="checked"> 
+                    <h4 class="checkbox3">Sunt de acord să primesc informaţii cu privire la promoţiile viitoare de la Canon şi partenerii săi.</h4>
+                    
+                    <div type="submit" data-buton="true" class="btn-trimite"><span class="btn-inscrie">Înscrie-te</span></div>
+                </form>
+            </div>
+
         </div>
 
     <!-- /body here -->
