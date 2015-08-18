a6f42d1212a8fc424b83ec230da6ba77718693a2
Mihai Barbu <xwave21@gmail.com>
2015-08-18 12:52:52 +0300

    Header si premii

---
+++ resources/views/tab.blade.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <title>Soho</title>
+        
+        <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/bootstrap-theme.min.css') }}">
+        
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+    </head>
+    <body>
+
+    <!-- body here -->
+
+        <div class="content">
+            <img src="/img/header-with-logo.png">
+            <img class="premii" src="/img/premii.png">
+            <p class="big-text">Aleg să-mi urmez visele.</p>
+            <p class="big-text-sec">Alegerea mea este Canon.</p>
+        </div>
+
+    <!-- /body here -->
+        
+        <script type="text/javascript" src="/js/lib/lodash.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <script src="/js/plugins.js"></script>
+        <script src="/js/main.js"></script>
+        
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-55513824-6']);
+          _gaq.push(['_trackPageview']);
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
+    </body>
+</html>
